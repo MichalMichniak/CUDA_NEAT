13be7bec9dcc4b1fdd1523fb2e88f34da34931cd
MichalMichniak <michalmichniak2001@gmail.com>
2024-12-28 18:44:08 +0100

    lekki update

--- crosoverCOO.cpp
+++ crosoverCOO.cpp
@@ -10,26 +10,36 @@ void countOffsprings(int *first_pair, int *second_pair, int no_offsprings, int *
         int idx_first = blocks_edges[first];
         int idx_second = blocks_edges[second];
         int acc = 0;
+        // if(i == 0) printf("\nstart\n");
         while(idx_first != blocks_edges[first+1] && idx_second != blocks_edges[second+1]){
+            // if(i == 0) printf("\ninnov: %d, %d\t %d, %d\n", innov[idx_first],innov[idx_second], idx_first, idx_second);
             if(innov[idx_first] == innov[idx_second]){
+                // if(i == 0) printf("%d", innov[idx_first]);
                 idx_first++;
                 idx_second++;
             }else if(innov[idx_first] < innov[idx_second]){
+                // if(i == 0) printf("%d\t", innov[idx_first]);
                 idx_first++;
             }else{
+                // if(i == 0) printf("%d\t", innov[idx_second]);
                 idx_second++;
             }
             acc++;
         }
+        // if(i == 0) printf("\tend\t");
         while(idx_first != blocks_edges[first+1]){
+            // if(i == 0) printf("%d\t", innov[idx_second]);
             idx_first++;
             acc++;
         }
         while(idx_second != blocks_edges[second+1]){
+            // if(i == 0) printf("%d\t", innov[idx_second]);
             idx_second++;
             acc++;
         }
         length_offspring[i+offset]=acc;
+        // if(i == 0) printf("\n%d\n", length_offspring[i+offset]);
+        // if(i == 0) printf("\nendend\n");
     }
 }
 
@@ -123,6 +133,7 @@ void crossover(
         while(idx_first != blocks_nodes[first+1] && idx_second != blocks_nodes[second+1]){
             if(translation[idx_first] == translation[idx_second]){
                 new_translation[idx + new_blocks_nodes[i+offset]] = translation[idx_first];
+
                 translation_t[idx_first] = idx;
                 translation_t[idx_second] = idx;
                 idx++;
@@ -157,61 +168,72 @@ void crossover(
         idx_first = blocks_edges[first];
         idx_second = blocks_edges[second];
         idx = 0;
+        // if(i==0) printf("\nSTART\n");
         while(idx_first != blocks_edges[first+1] && idx_second != blocks_edges[second+1]){
             
             if(innov[idx_first] == innov[idx_second]){ // innov - only innovation number differs edges
-                new_w[idx+new_blocks_edges[i+offset]] = w[idx_first];
-                new_enabled[idx+new_blocks_edges[i+offset]] = enabled[idx_first];
+                new_w[idx+new_blocks_edges[i+offset]] = (w[idx_first] + w[idx_second])/2;
+                new_enabled[idx+new_blocks_edges[i+offset]] = enabled[idx_first] && enabled[idx_second];
                 new_innov[idx+new_blocks_edges[i+offset]] = innov[idx_first];
-                new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_first]];
-                new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_first]];
+                // if(i==0) printf("%d, %d\t", in[idx_first]+blocks_nodes[first_pair[i]],translation_t[in[idx_first]]+blocks_nodes[first_pair[i]]);
+                new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_first]+blocks_nodes[first_pair[i]]];
+                new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_first]+blocks_nodes[first_pair[i]]];
                 idx_first++;
                 idx_second++;
                 idx++;
             }else if(innov[idx_first] < innov[idx_second]){
+                // if(i==0) printf("%d, %d\t", in[idx_first]+blocks_nodes[first_pair[i]],translation_t[in[idx_first]+blocks_nodes[first_pair[i]]]);
                 new_w[idx+new_blocks_edges[i+offset]] = w[idx_first];
                 new_enabled[idx+new_blocks_edges[i+offset]] = enabled[idx_first];
                 new_innov[idx+new_blocks_edges[i+offset]] = innov[idx_first];
-                new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_first]];
-                new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_first]];
+                new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_first]+blocks_nodes[first_pair[i]]];
+                new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_first]+blocks_nodes[first_pair[i]]];
                 idx_first++;
                 idx++;
             }else{
+                // if(i==0) printf("%d, %d\t", in[idx_second]+blocks_nodes[second_pair[i]],translation_t[in[idx_second]+blocks_nodes[second_pair[i]]]);
                 new_w[idx+new_blocks_edges[i+offset]] = w[idx_second];
                 new_enabled[idx+new_blocks_edges[i+offset]] = enabled[idx_second];
                 new_innov[idx+new_blocks_edges[i+offset]] = innov[idx_second];
-                new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_second]];
-                new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_second]];
+                new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_second]+blocks_nodes[second_pair[i]]];
+                new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_second]+blocks_nodes[second_pair[i]]];
                 idx_second++;
                 idx++;
             }
 
         }
         while(idx_first != blocks_edges[first+1]){
+            // if(i==0) printf("%d, %d\t", in[idx_first]+blocks_nodes[first_pair[i]],translation_t[in[idx_first]+blocks_nodes[first_pair[i]]]);
             new_w[idx+new_blocks_edges[i+offset]] = w[idx_first];
             new_enabled[idx+new_blocks_edges[i+offset]] = enabled[idx_first];
             new_innov[idx+new_blocks_edges[i+offset]] = innov[idx_first];
-            new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_first]];
-            new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_first]];
+            new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_first]+blocks_nodes[first_pair[i]]];
+            new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_first]+blocks_nodes[first_pair[i]]];
             idx_first++;
             idx++;
         }
         while(idx_second != blocks_edges[second+1]){
+            // if(i==0) printf("%d, %d\t", in[idx_second]+blocks_nodes[second_pair[i]],translation_t[in[idx_second]+blocks_nodes[second_pair[i]]]);
             new_w[idx+new_blocks_edges[i+offset]] = w[idx_second];
             new_enabled[idx+new_blocks_edges[i+offset]] = enabled[idx_second];
             new_innov[idx+new_blocks_edges[i+offset]] = innov[idx_second];
-            new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_second]];
-            new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_second]];
+            new_in[idx+new_blocks_edges[i+offset]] = translation_t[in[idx_second]+blocks_nodes[second_pair[i]]];
+            new_out[idx+new_blocks_edges[i+offset]] = translation_t[out[idx_second]+blocks_nodes[second_pair[i]]];
             idx_second++;
             idx++;
         }
+        // if(i==0) printf("\nEND\n");
     }
-
-
+    
 }
 
 
 void test_crosover(){
+    FILE *plik = fopen("crosoverCOO_test.txt", "r");
+    if (plik == NULL) {
+        return;
+    }
+
     int *in;
     int *out;
     float *w;
@@ -229,10 +251,73 @@ void test_crosover(){
     int no_offsprings; // ze starej populacji
     int *first_pair; // first parent: size no_offsprings
     int *second_pair; // second parent: size no_offsprings
-    int *length_offspring; // size no_offsprings
-    int *length_offspring_nodes; // size no_offsprings
+    // init
+    fscanf(plik, "%d", &no_instances);
+    blocks_nodes = (int*) malloc((no_instances+1) * sizeof(int));
+    for(int i = 0; i<no_instances+1; i++){
+        fscanf(plik, "%d", blocks_nodes+i);
+    }
+    blocks_edges = (int*) malloc((no_instances+1) * sizeof(int));
+    for(int i = 0; i<no_instances+1; i++){
+        fscanf(plik, "%d", blocks_edges+i);
+    }
+    translation = (int*) malloc((blocks_nodes[no_instances]) * sizeof(int));
+    for(int i = 0; i<blocks_nodes[no_instances]; i++){
+        fscanf(plik, "%d", translation+i);
+    }
+    innov = (int*) malloc((blocks_edges[no_instances]) * sizeof(int));
+    for(int i = 0; i<blocks_edges[no_instances]; i++){
+        fscanf(plik, "%d", innov+i);
+    }
+    enabled = (bool*) malloc((blocks_edges[no_instances]) * sizeof(int));
+    for(int i = 0; i<blocks_edges[no_instances]; i++){
+        int temp;
+        fscanf(plik, "%d", &temp);
+        *(enabled+i) = (bool)temp;
+    }
 
-    
+    in = (int*) malloc((blocks_edges[no_instances]) * sizeof(int));
+    for(int i = 0; i<blocks_edges[no_instances]; i++){
+        fscanf(plik, "%d", in+i);
+    }
+    out = (int*) malloc((blocks_edges[no_instances]) * sizeof(int));
+    for(int i = 0; i<blocks_edges[no_instances]; i++){
+        fscanf(plik, "%d", out+i);
+    }
+    w = (float*) malloc((blocks_edges[no_instances]) * sizeof(float));
+    for(int i = 0; i<blocks_edges[no_instances]; i++){
+        fscanf(plik, "%f", w+i);
+    }
+    fscanf(plik, "%d", &no_survivors);
+    mask = (int*) malloc(no_instances * sizeof(int));
+    for(int i = 0; i<no_instances; i++){
+        fscanf(plik, "%d", mask+i);
+    }
+    fscanf(plik, "%d", &no_offsprings);
+    first_pair = (int*) malloc(no_offsprings * sizeof(int));
+    for(int i = 0; i<no_offsprings; i++){
+        fscanf(plik, "%d", first_pair+i);
+    }
+    second_pair = (int*) malloc(no_offsprings * sizeof(int));
+    for(int i = 0; i<no_offsprings; i++){
+        fscanf(plik, "%d", second_pair+i);
+    }
+    // end of init
+    printf("\nold block nodes: ");
+
+    for(int i=0; i<6; i++){
+        printf("%d\t", blocks_nodes[i]);
+    }
+    printf("\nold block edges: ");
+    for(int i=0; i<6; i++){
+        printf("%d\t", blocks_edges[i]);
+    }
+    printf("\ninnovation numbers: ");
+    for(int i=0; i<24; i++){
+        printf("%d\t", innov[i]);
+    }
+    printf("\n");
+
     int *istance_numbers_seq; // tablica numeru instancji mapowanej ze starej do nowej tablicy szie: no_survivors
     istance_numbers_seq = (int*)malloc(no_survivors*sizeof(int));
     int idx = 0;
@@ -242,15 +327,22 @@ void test_crosover(){
             idx++;
         }
     }
+    
     int *new_blocks_nodes;
     int *new_blocks_edges;
     new_blocks_nodes = (int*)malloc((1+no_survivors+no_offsprings+no_mutations)*sizeof(int)); // 0 | 1 do no_survivors | no_survivors + 1 do no_survivors + no_offsprings | no_survivors + no_offsprings + 1 do no_survivors + no_offsprings + no_mutation 
     new_blocks_edges = (int*)malloc((1+no_survivors+no_offsprings+no_mutations)*sizeof(int)); 
+    new_blocks_edges[0] = 0;
+    new_blocks_nodes[0] = 0;
+    
     countSurvivors(blocks_edges, blocks_nodes, new_blocks_edges, new_blocks_nodes, no_survivors, istance_numbers_seq); // uzupełnienie rozmiarów instancji survivors  (0 | 1 do no_survivors)
+
     countOffsprings(first_pair, second_pair, no_offsprings, innov, blocks_edges, no_instances, new_blocks_edges, no_survivors + 1);
+    
     countOffspringsNodes(first_pair, second_pair, no_offsprings, translation, blocks_nodes, no_instances, new_blocks_nodes, no_survivors + 1);
     // TODO: mutations
-
+        
+    
     for(int i = 0; i<no_survivors+no_offsprings+no_mutations; i++){// liczenie histogramu skumulowanego (można zrównoleglić)
         new_blocks_nodes[i+1] = new_blocks_nodes[i+1] + new_blocks_nodes[i];
         new_blocks_edges[i+1] = new_blocks_edges[i+1] + new_blocks_edges[i];
@@ -270,10 +362,10 @@ void test_crosover(){
     new_enabled = (bool*)malloc(new_blocks_edges[new_no_instances]*sizeof(bool));
     new_innov = (int*)malloc(new_blocks_edges[new_no_instances]*sizeof(int));
     new_translation = (int*)malloc(new_blocks_nodes[new_no_instances]*sizeof(int));
-
     // tytaj będzie przepisywanie survivorsów
     copySurvivors(istance_numbers_seq, blocks_edges, blocks_nodes, new_blocks_nodes, new_blocks_edges, no_survivors,
     new_in, new_out, new_w, new_enabled, new_innov, new_translation, in, out, w, enabled, innov, translation);
+    
     // tytaj będzie tworzenie potomków
     int *translation_t;
     translation_t = (int*)malloc(blocks_nodes[no_instances]*sizeof(int)); // temporary translation table for crossover
@@ -301,7 +393,44 @@ void test_crosover(){
     translation_t // jako __shared__ ?
     );
 
+    printf("\nnew block nodes: ");
 
+    for(int i=0; i<7; i++){
+        printf("%d\t", new_blocks_nodes[i]);
+    }
+    printf("\nnew block edges: ");
+    for(int i=0; i<7; i++){
+        printf("%d\t", new_blocks_edges[i]);
+    }
+    printf("\ntemp translation: ");
+    for(int i=0; i<blocks_nodes[no_instances]; i++){
+        printf("%d\t", translation_t[i]);
+    }
+    printf("\nnew translation: ");
+    for(int i=0; i<new_blocks_nodes[no_survivors+no_offsprings+no_mutations]; i++){
+        printf("%d\t", new_translation[i]);
+    }
+    printf("\nnew in: ");
+    for(int i=0; i<new_blocks_edges[no_survivors+no_offsprings+no_mutations]; i++){
+        printf("%d\t", new_in[i]);
+    }
+
+    printf("\nnew out: ");
+    for(int i=0; i<new_blocks_edges[no_survivors+no_offsprings+no_mutations]; i++){
+        printf("%d\t", new_out[i]);
+    }
+
+    printf("\nnew innov: ");
+    for(int i=0; i<new_blocks_edges[no_survivors+no_offsprings+no_mutations]; i++){
+        printf("%d\t", new_innov[i]);
+    }
+
+    printf("\nnew enabled: ");
+    for(int i=0; i<new_blocks_edges[no_survivors+no_offsprings+no_mutations]; i++){
+        printf("%d\t", (int)new_enabled[i]);
+    }
+    printf("\n");
+    // tutaj free TODO:
 
 }
 
--- toCSRmatrix_with_enable.cpp
+++ toCSRmatrix_with_enable.cpp
@@ -2,35 +2,43 @@
 #include <stdlib.h>
 
 
-void updateRowPointers(int *row_pointers, int *out, int *blocks_edges, int *blocks_nodes, int no_instances){
+void updateRowPointers(int *row_pointers, int *out, int *blocks_edges, int *blocks_nodes, int no_instances, bool* enable){
     int idx = 0; // granulacja na poziomie idx per kernel
     for(int i=0; i<blocks_edges[no_instances]; i++){
-        while(blocks_edges[idx+1]<=i) idx++;
-        row_pointers[(out[i])+blocks_nodes[idx]+1] +=1;
+        while(blocks_edges[idx+1]<=i) idx++;// granica pomiędzy kernelami (warunek do pętli for)
+        if (enable[i])
+            row_pointers[(out[i])+blocks_nodes[idx]+1] +=1;
     }
     for(int i=0; i<blocks_nodes[no_instances]; i++){
         row_pointers[i+1] += row_pointers[i];
     }
 }
 
-void updateCol_idx_weights(int *row_pointers_t, float *weights, int *col_idx, int *in, float *w, int *out, int *blocks_edges, int *blocks_nodes, int no_instances){
+void updateCol_idx_weights(int *row_pointers_t, float *weights, int *col_idx, int *in, float *w, int *out, int *blocks_edges, int *blocks_nodes, int no_instances, bool* enable){
     int idx = 0; // granulacja na poziomie idx per kernel
+
     for(int i=0; i<blocks_edges[no_instances]; i++){
         while(blocks_edges[idx+1]<=i) idx++;
-        int temp = row_pointers_t[out[i] +blocks_nodes[idx]];
-        row_pointers_t[out[i]+blocks_nodes[idx]] +=1; // atomicAdd
-        col_idx[temp] = in[i]+blocks_nodes[idx]; // jeżeli będą podwójne to nie ma znaczenia przy mnożeniu macierzowym
-        weights[temp] = w[i];
+        if (enable[i]){
+            int temp = row_pointers_t[out[i] +blocks_nodes[idx]];
+            row_pointers_t[out[i]+blocks_nodes[idx]] +=1; // atomicAdd
+            col_idx[temp] = in[i]+blocks_nodes[idx]; // jeżeli będą podwójne to nie ma znaczenia przy mnożeniu macierzowym
+            weights[temp] = w[i];
+        }
     }
 }
-
+int count_col_size(bool* enable, int length){
+    int acc = 0;
+    for(int i=0; i<length; i++) if (enable[i]) acc++;
+    return acc;
+}
 
 int testToCSR(){
 
     int *in;
     int *out;
     float *w;
-    bool *enabled; // not yet implemented
+    bool *enabled; // implemented as enable below
     int *innov; // not yet implemented
     
     int no_instances;
@@ -62,21 +70,20 @@ int testToCSR(){
     for(int i = 0; i<blocks_edges[no_instances]; i++){
         fscanf(plik, "%f", w+i);
     }
-
-
-
+    bool enable[8] = {false,false,true,false,true,true,true,true};
+    int colsize = count_col_size(enable, 8);
 
     int *col_idx; // size w
     float *weights; // size w
     int *row_pointers; //size translation+1
     
-    col_idx = (int*) malloc((blocks_edges[no_instances]) * sizeof(int));
-    weights = (float*) malloc((blocks_edges[no_instances]) * sizeof(float));
+    col_idx = (int*) malloc((colsize) * sizeof(int));
+    weights = (float*) malloc((colsize) * sizeof(float));
     row_pointers = (int*) malloc((blocks_nodes[no_instances] + 1) * sizeof(int));
     for(int i = 0; i<(blocks_nodes[no_instances] + 1); i++){
         row_pointers[i] = 0;
     }
-    updateRowPointers(row_pointers, out, blocks_edges, blocks_nodes, no_instances);
+    updateRowPointers(row_pointers, out, blocks_edges, blocks_nodes, no_instances, enable);
 
     int *row_pointers_t;
     row_pointers_t = (int*) malloc((blocks_nodes[no_instances] + 1) * sizeof(int));
@@ -84,17 +91,17 @@ int testToCSR(){
         row_pointers_t[i] = row_pointers[i];
     }
 
-    updateCol_idx_weights(row_pointers_t, weights, col_idx, in, w, out, blocks_edges, blocks_nodes, no_instances);
+    updateCol_idx_weights(row_pointers_t, weights, col_idx, in, w, out, blocks_edges, blocks_nodes, no_instances,enable);
 
     for(int i=0; i<(blocks_nodes[no_instances] + 1); i++){
         printf("%d\t", row_pointers[i]);
     }
     printf("\n");
-    for(int i=0; i<(blocks_edges[no_instances]); i++){
+    for(int i=0; i<(colsize); i++){
         printf("%d\t", col_idx[i]);
     }
     printf("\n");
-    for(int i=0; i<(blocks_edges[no_instances]); i++){
+    for(int i=0; i<(colsize); i++){
         printf("%f\t", weights[i]);
     }
 
